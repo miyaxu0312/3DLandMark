c43840d8eb88c4895f52b503ca0ecf1590547c1c
miyaxu0312 <39933311+miyaxu0312@users.noreply.github.com>
2018-07-23 16:48:59 +0800

    Update pre_process.cpp

--- pre_process.cpp
+++ pre_process.cpp
@@ -103,7 +103,7 @@ void pre_process(string filePath, string boxPath, string netOutPath, string post
         
         warpAffine(img, similar_img, affine_mat,  similar_img.size());
         /*save pre-processed image for the network*/
-	if (access(savePath,6)==-1)
+	if (_access(savePath,6)==-1)
    	{
     	 	mkdir(savePath);
     	}
