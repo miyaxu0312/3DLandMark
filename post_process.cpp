6ddcc79a03279c4d1ede74a72c2bc01e79393741
miyaxu0312 <39933311+miyaxu0312@users.noreply.github.com>
2018-07-23 17:21:41 +0800

    Update post_process.cpp

--- post_process.cpp
+++ post_process.cpp
@@ -98,7 +98,7 @@ void post_process(string ori_path, string filePath, string save_path, string pos
             }
             
         }
-	if (access(save_path,6)==-1)
+	if (access(save_path.c_str(),6)==-1)
     	{
      	    mkdir(save_path);
     	}
